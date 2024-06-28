e1cdecc3725d01bf8c4ebc1bfa4bf49269c70901
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-06-28 09:09:03 +0200

    feat(js) script for the page completed

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,34 @@
+const { createApp } = Vue;
 
+createApp({
+    data() {
+        return {
+            users: [],
+        }
+    },
+
+    methods: {
+        assignStringAttributeFromApi(pathToGet , userObject , userAttributeString) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/' + pathToGet)
+            .then(callReturn => {
+                userObject[`'${userAttributeString}'`] = callReturn.data.response;
+            });
+        },
+    },
+
+    computed: {
+        getUsersArray() {
+            return this.users;
+        },
+    },
+
+    mounted() {
+        for(let i = 0 ; i < 10 ; i++) {
+            const user = {};
+            this.assignStringAttributeFromApi('name' , user , 'fullName');
+            this.assignStringAttributeFromApi('mail' , user , 'email');
+            this.users.push(user);
+        }
+        console.log(this.users);
+    }
+}).mount('#app');
